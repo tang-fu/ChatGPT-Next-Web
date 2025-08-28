1a10fd65b94ef466f8349f3d89d15a8b98901e69
tang-fu <73867974+tang-fu@users.noreply.github.com>
2025-08-28 22:42:36 +0100

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -246,11 +246,15 @@ export class ChatGPTApi implements LLMApi {
         stream: options.config.stream,
         model: modelConfig.model,
         //*****************************************修改部分*******************************************/
-        temperature: (!isO1OrO3 && !isGpt5Core) ? modelConfig.temperature : 1,
+        //temperature: (!isO1OrO3 && !isGpt5Core) ? modelConfig.temperature : 1,
+        //presence_penalty: !isO1OrO3 ? modelConfig.presence_penalty : 0,
+        //frequency_penalty: !isO1OrO3 ? modelConfig.frequency_penalty : 0,
+        //top_p: !isO1OrO3 ? modelConfig.top_p : 1,
+        temperature: !(isO1OrO3 || isGpt5Family) ? modelConfig.temperature : 1,
+        presence_penalty: !(isO1OrO3 || isGpt5Family) ? modelConfig.presence_penalty : 0,
+        frequency_penalty: !(isO1OrO3 || isGpt5Family) ? modelConfig.frequency_penalty : 0,
+        top_p: !(isO1OrO3 || isGpt5Family) ? modelConfig.top_p : 1,
         //****************************************************************************************/
-        presence_penalty: !isO1OrO3 ? modelConfig.presence_penalty : 0,
-        frequency_penalty: !isO1OrO3 ? modelConfig.frequency_penalty : 0,
-        top_p: !isO1OrO3 ? modelConfig.top_p : 1,
         // max_tokens: Math.max(modelConfig.max_tokens, 1024),
         // Please do not ask me why not send max_tokens, no reason, this param is just shit, I dont want to explain anymore.
       };
